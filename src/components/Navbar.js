0b8251ef4328880d309fd91cc43460d8b4784bf3
glitton <glcodeworks@gmail.com>
2023-01-10 16:10:52 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+      <div class="nav-center">
+        <div class="nav-header">
+          <img src={logo} class="nav-logo" alt="backroads" />
+          <button type="button" class="nav-toggle" id="nav-toggle">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        <ul class="nav-links" id="nav-links">
+          {pageLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} class="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul class="nav-icons">
+          <li>
+            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+              <i class="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+              <i class="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com" target="_blank" class="nav-icon">
+              <i class="fab fa-squarespace"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
